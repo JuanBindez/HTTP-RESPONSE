0745fea0cdf20b8f48c3705210049ce8c3d91184
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-03-03 08:36:59 -0300

    Update resposta.py

--- resposta.py
+++ resposta.py
@@ -1,4 +1,8 @@
+"""pip install requests
+   pip install beautifulsoup4"""
+
 import requests
+from bs4 import BeautifulSoup
 
 
 def status_da_response():
@@ -13,6 +17,10 @@ def cabecalho_da_pagina():
 def conteudo_da_pagina():
     print('\n Content')
     print(response.content)
+    
+def find_tag():
+    soup = str(input("digite a tag html procurada:\n"))
+    BeautifulSoup(soup)
 
 
 link = str(input('digite um link:\n'))
@@ -21,4 +29,5 @@ response = requests.get(link)
 status_da_response()
 cabecalho_da_pagina()
 #conteudo_da_pagina()
+find_tag()
 
